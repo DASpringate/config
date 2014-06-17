c06e3003cd809799644939e3c1305d26424997fc
David Springate <daspringate@gmail.com>
2014-06-17 16:22:01 +0100

    improved clojure options

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -26,10 +26,12 @@
 (require 'dired-x)
 (require 'compile)
 
+
+
 (setq visible-bell t)
 (blink-cursor-mode)
 (ido-mode t)
-;(menu-bar-mode 0)
+(menu-bar-mode 0)
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
 (put 'downcase-region 'disabled nil)
@@ -37,7 +39,7 @@
 (setq column-number-mode t)
 (setq inhibit-startup-message t)
 (setq-default major-mode 'org-mode)
-(setq initial-scratch-message "scratch")
+(setq initial-scratch-message "***scratch***\n\n")
 (setq save-abbrevs nil)
 (setq show-trailing-whitespace t)
 (setq suggest-key-bindings t)
@@ -49,19 +51,18 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit autoface-default :strike-through nil :underline nil :slant normal :weight normal :height 120 :width normal :family "monaco"))))
- '(column-marker-1 ((t (:background "red"))))
- '(diff-added ((t (:foreground "cyan"))))
+ '(column-marker-1 ((t (:background "red"))) t)
+ '(diff-added ((t (:foreground "cyan"))) t)
  '(flymake-errline ((((class color) (background light)) (:background "Red"))))
  '(font-lock-comment-face ((((class color) (min-colors 8) (background light)) (:foreground "red"))))
- '(fundamental-mode-default ((t (:inherit default))))
+ '(fundamental-mode-default ((t (:inherit default))) t)
  '(highlight ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
  '(isearch ((((class color) (min-colors 8)) (:background "yellow" :foreground "black"))))
  '(linum ((t (:foreground "black" :weight bold))))
  '(region ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
  '(secondary-selection ((((class color) (min-colors 8)) (:background "gray" :foreground "cyan"))))
  '(show-paren-match ((((class color) (background light)) (:background "black"))))
- '(vertical-border ((t nil)))
-)
+ '(vertical-border ((t nil))))
 
 ;; ------------
 ;; -- Macros --
@@ -135,11 +136,12 @@
 
 (load-theme 'tsdh-dark t)
 
+(setq backup-directory-alist '(("." . "~/.saves")))
 
 ;; lisp stuff
 
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
-(setq inferior-lisp-program "/usr/bin/sbcl")
+(setq inferior-lisp-program "~/bin/sbcl")
 (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
 (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
 (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
@@ -152,6 +154,10 @@
 (load "/home/david/quicklisp/clhs-use-local.el" t)
 
 
+;; clojure stuff
+
+(load "~/.emacs.d/vendor/clojure")
+
 ;; org mode 
 
 (global-set-key "\C-cl" 'org-store-link)
@@ -163,10 +169,9 @@
 
 
 
-
-
-
-
-
-
-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Dropbox/notes/together.org" "~/Dropbox/notes/work.org"))))
